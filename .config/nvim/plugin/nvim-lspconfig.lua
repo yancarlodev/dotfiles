adf6941f3e5a6d95347261b4bbead58897fee736
Yan Lepri <yancarlodc@gmail.com>
2026-06-11 18:41:34 -0300

    feat(lsp): setup diagnostics

--- .config/nvim/plugin/nvim-lspconfig.lua
+++ .config/nvim/plugin/nvim-lspconfig.lua
@@ -1,2 +1,11 @@
 vim.pack.add({ 'https://github.com/neovim/nvim-lspconfig' })
-vim.lsp.enable({ 'lua_ls', 'gopls' })
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = true,
+  severity_sort = true,
+})
+
+vim.lsp.enable({ 'lua_ls', 'gopls', 'ts_ls' })
