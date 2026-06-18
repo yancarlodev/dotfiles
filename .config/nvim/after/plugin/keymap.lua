d5d51cc770ccd8989aa7daab2a5c334ae2ec92e5
Yan Lepri <yancarlodc@gmail.com>
2026-06-18 19:35:37 -0300

    refactor(nvim): change from self-managed to astro

--- .config/nvim/after/plugin/keymap.lua
+++
@@ -1,7 +0,0 @@
-vim.g.mapleader = ' '
-
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
-vim.keymap.set("n", "<space>x", ":.lua<CR>")
-vim.keymap.set("v", "<space>x", ":lua<CR>")
-
-vim.keymap.set('n', '<leader>e', MiniFiles.open, { silent = true })
--- .config/nvim/plugin/color.lua
+++
@@ -1,4 +0,0 @@
-vim.pack.add({ 'https://github.com/ellisonleao/gruvbox.nvim' })
-
-vim.o.background = 'dark'
-vim.cmd.colorscheme('gruvbox')
